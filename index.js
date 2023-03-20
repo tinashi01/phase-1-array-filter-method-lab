9c3da8c94f4262e6ee04182ffd9a6ba41e9dceb0
Tina Shi <init.defaultBranch>
2023-03-21 00:18:04 +1100

    commit tree 75af3a4faf539b55517ed8ef9fd099b81559b2b8

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solution here
+function findMatching(drivers, name) {
+    for (const driver of drivers) {
+        if (name == driver.name) {
+            console.log(name);
+        }
+    }
+}
